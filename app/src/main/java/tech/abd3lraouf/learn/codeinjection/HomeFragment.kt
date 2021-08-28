50a448f089427ef90d76cdb0ea096dd8cb08f641
AbdElraoufSabri <abdelraoufsabri@gmail.com>
2021-08-28 20:46:14 +0200

    Initial commit

---
+++ app/src/main/java/tech/abd3lraouf/learn/codeinjection/HomeFragment.kt
@@ -0,0 +1,56 @@
+package tech.abd3lraouf.learn.codeinjection
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebView.setWebContentsDebuggingEnabled
+import android.webkit.WebViewClient
+import tech.abd3lraouf.learn.codeinjection.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.webView.apply {
+            settings.javaScriptEnabled = true
+            setWebContentsDebuggingEnabled(true)
+            JavaScriptShareInterface.bind(requireContext(), this)
+            webViewClient = CustomWebViewClient
+
+            loadUrl("https://abdelraoufsabri.github.io/webview_code_injection_from_android")
+        }
+    }
+
+    override fun onDestroyView() {
+        JavaScriptShareInterface.unbind(binding.webView)
+        _binding = null
+        super.onDestroyView()
+    }
+
+    object CustomWebViewClient : WebViewClient(){
+        override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            JavaScriptShareInterface.injectMediator(view)
+        }
+    }
+}
---
+++ app/src/main/java/tech/abd3lraouf/learn/codeinjection/JavaScriptShareInterface.kt
@@ -0,0 +1,51 @@
+package tech.abd3lraouf.learn.codeinjection
+
+import android.content.Context
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.widget.Toast
+
+class JavaScriptShareInterface(private val context: Context) {
+
+    @JavascriptInterface
+    @Suppress("unused")
+    fun toast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        /**
+         * key used by Javascript to call the native function on
+         */
+        private const val JS_KEY = "Android"
+
+        /**
+         * key used as a wrapper key, not visible in Javascript
+         */
+        private const val JS_MEDIATOR_KEY = "JsMediator"
+
+        /**
+         * called at onCreate to initiate code injection
+         */
+        fun bind(context: Context, webView: WebView) {
+            webView.addJavascriptInterface(JavaScriptShareInterface(context), JS_MEDIATOR_KEY)
+        }
+
+        /**
+         * called in on destroy view to clear javascript interfaces for security reasons
+         */
+        fun unbind(webView: WebView) {
+            webView.removeJavascriptInterface(JS_MEDIATOR_KEY)
+        }
+
+        /**
+         * called before start loading the page, for better handling
+         */
+        fun injectMediator(webView: WebView) {
+            webView.evaluateJavascript(
+                "$JS_KEY = {}; $JS_KEY.toast = function(message) { $JS_MEDIATOR_KEY.toast(message); }",
+                null
+            )
+        }
+    }
+}
